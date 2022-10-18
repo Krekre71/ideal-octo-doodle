0c99f62c6c3213848dde002dd27e2cd4a88287a3
Krekre71 <113843340+Krekre71@users.noreply.github.com>
2022-10-18 21:17:31 +0200

    Update PCE_LOT_1.py

--- PCE_LOT_1.py
+++ PCE_LOT_1.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="PCE_MI",
                    layout="wide"
 )
 
-adresse = "https://github.com/Krekre71/ideal-octo-doodle/blob/main/CW2020_ExportACV_Version%201-Maison%20individuelle_2022.xlsx"
+adresse = "C:\\Users\\Jean-David\\OneDrive\\Bureau\\Python\\CSV PANDAS\\CSV CW2020\\CW2020_ExportACV_Version 1-Maison individuelle_2022.xlsx"
 
 df_read = pd.read_excel(adresse,
                    engine='openpyxl',
